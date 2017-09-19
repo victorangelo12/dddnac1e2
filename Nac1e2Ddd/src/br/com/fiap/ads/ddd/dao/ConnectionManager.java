081e57639f761112b337c5ba45402e3fbacd9aad
logonrm <logonrm@L2601MICRO57.fiap.com.br>
2017-09-18 22:10:15 -0300

    Criação do ConnectionManager

--- Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/ConnectionManager.java
+++ Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/ConnectionManager.java
@@ -1,5 +1,54 @@
 package br.com.fiap.ads.ddd.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+	// instancia unica
+
+	private static ConnectionManager instance;
+
+	private ConnectionManager() throws ClassNotFoundException {
+
+		// registra o drive jdbc
+
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+	}
+
+	public static ConnectionManager getInstance() throws SQLException {
+		 		
+		try {
+			// verifica se ja uma conexão
+			if (instance == null) {
+				instance = new ConnectionManager();
+			}
+
+		} catch (ClassNotFoundException e) {
+			throw new SQLException("O driver JBC não foi encontrado "+e.getMessage());
+		}
+		return instance;
+
+	}
 	
+	//abre a conexão com o banco
+	
+	public Connection getConnection()throws SQLException{
+		//parametros da conexão
+		
+		String usuario = "rm77917"; //rm00000
+		String senha = "200997"; //DDMMAA
+		String jdbcUrl = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL"; //acesso externo
+		
+		//abre a conexão 
+		
+		try {
+			return DriverManager.getConnection(jdbcUrl,usuario,senha);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new SQLException("Erro ao se conectar com o banco de dados");
+		}
+		
+	}
+
 }
