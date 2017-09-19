ea0c3c63adc38ad73529617693fd813231395c1b
vitao <vitao@DESKTOP-UNPO9NJ>
2017-09-19 15:19:31 -0300

    Criação do PreparedStatement, passando valores para java,atribuindo aos atributos

--- Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/VeiculoDAO.java
+++ Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/VeiculoDAO.java
@@ -1,5 +1,27 @@
 package br.com.fiap.ads.ddd.dao;
 
-public class VeiculoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.com.fiap.ads.ddd.to.Veiculo;
+
+public class VeiculoDAO {
+	public void incluir(Veiculo veiculo) throws SQLException{
+		Connection conn = null;
+		
+		conn = ConnectionManager.getInstance().getConnection();
+		
+		PreparedStatement stmtInsert = conn.prepareStatement("INSERT INTO TB_ALUNO"
+														+ "(id_veiculo,modelo,placa,ano,motor)"+"VALUES"
+														+"(sq_veiculo.NEXTVAL,?,?,?,?)");
+		
+		stmtInsert.setString(1, veiculo.getModelo());
+		stmtInsert.setString(2,veiculo.getPlaca());
+		stmtInsert.setInt(3, veiculo.getAno());
+		stmtInsert.setDouble(4, veiculo.getMotor());
+		
+		stmtInsert.executeUpdate();
+														
+	}
 }
