0e1455f3bd3929407379393327851251640e42f3
vitao <vitao@DESKTOP-UNPO9NJ>
2017-09-19 17:43:57 -0300

    Finalização Incluir na DAO e criação apenas da classe BO

---
+++ Nac1e2Ddd/src/br/com/fiap/ads/ddd/bo/VeiculoBO.java
@@ -0,0 +1,5 @@
+package br.com.fiap.ads.ddd.bo;
+
+public class VeiculoBO {
+	
+}
--- Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/VeiculoDAO.java
+++ Nac1e2Ddd/src/br/com/fiap/ads/ddd/dao/VeiculoDAO.java
@@ -7,21 +7,37 @@ import java.sql.SQLException;
 import br.com.fiap.ads.ddd.to.Veiculo;
 
 public class VeiculoDAO {
-	public void incluir(Veiculo veiculo) throws SQLException{
+		//INCLUIR VEICULO
+	public void incluir(Veiculo veiculo) throws SQLException {
+		
 		Connection conn = null;
 		
-		conn = ConnectionManager.getInstance().getConnection();
-		
-		PreparedStatement stmtInsert = conn.prepareStatement("INSERT INTO TB_ALUNO"
-														+ "(id_veiculo,modelo,placa,ano,motor)"+"VALUES"
-														+"(sq_veiculo.NEXTVAL,?,?,?,?)");
-		
-		stmtInsert.setString(1, veiculo.getModelo());
-		stmtInsert.setString(2,veiculo.getPlaca());
-		stmtInsert.setInt(3, veiculo.getAno());
-		stmtInsert.setDouble(4, veiculo.getMotor());
-		
-		stmtInsert.executeUpdate();
-														
+		try {
+
+			conn = ConnectionManager.getInstance().getConnection();
+
+			PreparedStatement stmtInsert = conn.prepareStatement("INSERT INTO TB_ALUNO"
+					+ "(id_veiculo,modelo,placa,ano,motor)" + "VALUES" + "(sq_veiculo.NEXTVAL,?,?,?,?)");
+
+			stmtInsert.setString(1, veiculo.getModelo());
+			stmtInsert.setString(2, veiculo.getPlaca());
+			stmtInsert.setInt(3, veiculo.getAno());
+			stmtInsert.setDouble(4, veiculo.getMotor());
+
+			stmtInsert.executeUpdate();
+
+		} catch (SQLException e) {
+			System.err.println("Erro ao inserir veiculo");
+
+		} finally {
+	
+				if (conn != null) // condição se tem uma conexão aberta
+					try {
+					conn.close(); // fecha
+			} catch (SQLException e2) {
+				System.err.println("Erro ao fechar conexão");
+			}
+		}
+
 	}
 }
